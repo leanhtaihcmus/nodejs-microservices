a2207f6bf01ca232218bbf98a3c0170db6202e0b
Lê Anh Tài <leanhtaihcmus@gmail.com>
2022-10-10 13:13:24 +0700

    Update log of event-bus to prove posts-srv and event-bus-srv communicated

--- blog/event-bus/index.js
+++ blog/event-bus/index.js
@@ -15,11 +15,12 @@ app.post('/events', (req, res) => {
   const event = req.body;
 
   events.push(event);
+  console.log(`Received Event ${event.type}`);
 
   axios.post('http://posts-clusterip-srv:4000/events', event).catch((err) => {
     console.log(err.message);
   });
-  axios.post('http://localhost:4001/events', event).catch((err) => {
+  /*axios.post('http://localhost:4001/events', event).catch((err) => {
     console.log(err.message);
   });
   axios.post('http://localhost:4002/events', event).catch((err) => {
@@ -27,7 +28,7 @@ app.post('/events', (req, res) => {
   });
   axios.post('http://localhost:4003/events', event).catch((err) => {
     console.log(err.message);
-  });
+  });*/
 
   res.send({ status: 'OK' });
 });
