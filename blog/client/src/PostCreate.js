6f32f4fed999b63a397a367740d2314b004c2e5b
Lê Anh Tài <leanhtaihcmus@gmail.com>
2022-10-11 22:05:07 +0700

    Config unique route paths in ingress

--- blog/client/src/PostCreate.js
+++ blog/client/src/PostCreate.js
@@ -7,7 +7,7 @@ const CreatePost = () => {
   const onSubmit = async (event) => {
     event.preventDefault();
 
-    await axios.post('http://posts.com/posts', {
+    await axios.post('http://posts.com/posts/create', {
       title
     });
 
