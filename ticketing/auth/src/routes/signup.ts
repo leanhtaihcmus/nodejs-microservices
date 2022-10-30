95389127da0cf8dd8a8a8c24574b042693fdb61f
Lê Anh Tài <leanhtaihcmus@gmail.com>
2022-10-30 14:04:09 +0700

    Adding validation and Revert to local Skaffold

--- ticketing/auth/src/routes/signup.ts
+++ ticketing/auth/src/routes/signup.ts
@@ -1,8 +1,19 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
 
 const router = express.Router();
 
-router.post('/api/users/signup', (req, res) => {
+router.post('/api/users/signup', 
+[
+  body('email')
+    .isEmail()
+    .withMessage('Email must be valid'),
+  body('password')
+    .trim()
+    .isLength({ min: 4, max: 20 })
+    .withMessage('Password must be between 4 and 20 characters')
+], 
+(req: Request, res: Response) => {
   res.send('Hi there!');
 });
 
