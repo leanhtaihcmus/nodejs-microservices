7768e3a41de0651d0f88fba1762071f72bf4f15f
Lê Anh Tài <leanhtaihcmus@gmail.com>
2022-11-04 11:45:16 +0700

    Abstract error and final error releated codes

--- ticketing/auth/src/errors/database-connection-error.ts
+++ ticketing/auth/src/errors/database-connection-error.ts
@@ -1,8 +1,11 @@
-export class DatabaseConnectionError extends Error {
+import { CustomError } from "./custom-error";
+
+export class DatabaseConnectionError extends CustomError {
+  statusCode = 500;
   reason = "Error connecting to database";
 
   constructor() {
-    super();
+    super('Database connection error');
 
     Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
   }
--- ticketing/auth/src/errors/request-validation-error.ts
+++ ticketing/auth/src/errors/request-validation-error.ts
@@ -1,17 +1,10 @@
 import { ValidationError } from "express-validator";
+import { CustomError } from "./custom-error";
 
-interface CustomError {
-  statusCode: number;
-  serializeErrors(): {
-    message: string;
-    field?: string;
-  }[];
-}
-
-export class RequestValidationError extends Error implements CustomError {
+export class RequestValidationError extends CustomError {
   statusCode = 400;
   constructor(public errors: ValidationError[]) {
-    super();
+    super('Request validation error');
 
     // Only because we extending a built in class
     Object.setPrototypeOf(this, RequestValidationError.prototype);
