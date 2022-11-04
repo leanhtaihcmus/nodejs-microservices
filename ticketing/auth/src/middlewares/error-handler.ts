bbd40c60719cefbd4650bc776ea6332b9c0ff3f3
Lê Anh Tài <leanhtaihcmus@gmail.com>
2022-11-04 09:17:56 +0700

    Convert errors to common response

--- ticketing/auth/src/middlewares/error-handler.ts
+++ ticketing/auth/src/middlewares/error-handler.ts
@@ -8,15 +8,19 @@ export const errorHandler = (
   res: Response, 
   next: NextFunction
 ) => {
+  // Follow up status code by https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
   if (err instanceof RequestValidationError) {
-    console.log('handling this error as a request validation error');
+    const formattedErrors = err.errors.map(error => {
+      return { message: error.msg, field: error.param };
+    });
+    return res.status(400).send({ errors: formattedErrors });
   }
 
   if (err instanceof DatabaseConnectionError) {
-    console.log('handling this error as a database connection error');
+    return res.status(500).send({ errors: [{ message: err.reason }] });
   }
 
   res.status(400).send({
-    message: err.message
+    errors: [{ message: 'Something went wrong' }]
   });
 };
