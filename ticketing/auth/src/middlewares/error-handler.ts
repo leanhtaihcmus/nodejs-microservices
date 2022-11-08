41eee42ad5306ecbb07eb65f98d632a8e3559f2d
Lê Anh Tài <leanhtaihcmus@gmail.com>
2022-11-08 20:44:48 +0700

    Creating database in kubernetes

--- ticketing/auth/src/middlewares/error-handler.ts
+++ ticketing/auth/src/middlewares/error-handler.ts
@@ -1,6 +1,4 @@
 import { Request, Response,  NextFunction } from 'express';
-import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
 import { CustomError } from '../errors/custom-error';
 
 export const errorHandler = (
