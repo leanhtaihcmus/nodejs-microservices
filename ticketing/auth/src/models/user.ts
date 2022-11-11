1081150e787d3d55ae959637b79fbb22b427b6af
Lê Anh Tài <leanhtaihcmus@gmail.com>
2022-11-11 19:39:43 +0700

    Type checking User properties

--- ticketing/auth/src/models/user.ts
+++ ticketing/auth/src/models/user.ts
@@ -1,5 +1,12 @@
 import mongoose from 'mongoose';
 
+// An interface that describes the properties
+// that are required to create a new user
+interface UserAttrs {
+  email: string;
+  password: string;
+}
+
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
@@ -13,4 +20,8 @@ const userSchema = new mongoose.Schema({
 
 const User = mongoose.model('User', userSchema);
 
-export { User };
+const buildUser = (attrs: UserAttrs) => {
+  return new User(attrs);
+};
+
+export { User, buildUser };
