b5b297c0689aa6958abfcbf3d8c0bb89e60f2c75
Lê Anh Tài <leanhtaihcmus@gmail.com>
2022-12-01 09:03:27 +0700

    Mongoose Pre-Save Hooks with hashed password

--- ticketing/auth/src/models/user.ts
+++ ticketing/auth/src/models/user.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import { Password } from '../services/password';
 
 // An interface that describes the properties
 // that are required to create a new user
@@ -34,6 +35,14 @@ const userSchema = new mongoose.Schema({
 userSchema.statics.build = (attrs: UserAttrs) => {
   return new User(attrs);
 };
+
+userSchema.pre('save', async function(done) {
+  if (this.isModified('password')) {
+    const hashed = await Password.toHash(this.get('password'));
+    this.set('password', hashed);
+  }
+  done();
+});
 
 const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
 
